b1219c5ba3949628bdcc88604f52e5a7a1f3a910
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-26 18:24:59 +0200

    05: add route to home

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import quiz.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', quiz.views.home, name='home'),
 ]
