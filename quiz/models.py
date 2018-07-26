7cf6549ce880e041f56be3af0ac534f1f27ef4be
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-26 19:25:36 +0200

    09: add __str__ method

--- quiz/models.py
+++ quiz/models.py
@@ -3,9 +3,15 @@ from django.db import models
 class Question(models.Model):
     label = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.label
+
 class Option(models.Model):
     label = models.CharField(max_length=100)
     question = models.ForeignKey(
         Question, on_delete=models.CASCADE, related_name='options'
     )
     is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.label
