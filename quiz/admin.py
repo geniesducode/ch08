5861e82bd2ded70c3239bb41098f3e311f083615
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-26 19:32:57 +0200

    10: use inlines

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from quiz.models import Option, Question
 
-admin.site.register(Option)
-admin.site.register(Question)
+class OptionInline(admin.TabularInline):
+    model = Option
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+admin.site.register(Question, QuestionAdmin)
