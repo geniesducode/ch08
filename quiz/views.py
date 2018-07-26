9d5a63a31be7e9443292c21635ee277eb2e2a323
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-26 21:53:25 +0200

    11: add question_detail view

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from quiz.models import Question
 
 def home(request):
     return render(request, 'quiz/home.html')
+
+def question_detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    context = {
+        'question': question,
+        'options': question.options.all(),
+        'nb_questions': Question.objects.count(),
+        'question_number': question.get_number(),
+    }
+    return render(request, 'quiz/question_detail.html', context)
