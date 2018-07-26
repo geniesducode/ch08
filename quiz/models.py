356cc3d35753a715d566d7720fdcbeb67e49f836
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-26 19:00:46 +0200

    07: add models and migrations

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    label = models.CharField(max_length=100)
+
+class Option(models.Model):
+    label = models.CharField(max_length=100)
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='options'
+    )
+    is_correct = models.BooleanField(default=False)
