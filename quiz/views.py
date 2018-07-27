ede7424b101ca4a22da4131979d2c026045f8655
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-27 15:10:12 +0200

    23: save answers in session

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from quiz.models import Question
 
 def home(request):
@@ -7,6 +7,21 @@ def home(request):
 
 def question_detail(request, question_id):
     question = get_object_or_404(Question, id=question_id)
+    if request.method == 'POST':
+        if 'answers' not in request.session:
+            request.session['answers'] = {}
+
+        request.session['answers'][str(question.id)] = (
+            request.POST.get('option')
+        )
+        request.session.modified = True
+        next_question = question.get_next_question()
+
+        if next_question:
+            return redirect('question_detail', question_id=next_question.id)
+        else:
+            return redirect('results')
+
     context = {
         'question': question,
         'options': question.options.all(),
