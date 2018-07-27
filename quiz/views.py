c45c8fc9d78ee693735e801fe799db354688b071
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-27 17:23:27 +0200

    27: add results view

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from quiz.models import Question
+from quiz.utils import get_rank
 
 def home(request):
     question = Question.objects.first()
@@ -29,3 +30,26 @@ def question_detail(request, question_id):
         'question_number': question.get_number(),
     }
     return render(request, 'quiz/question_detail.html', context)
+
+def results(request):
+    total_questions = Question.objects.count()
+    answered_questions = len(request.session.get('answers', {}))
+
+    if answered_questions != total_questions:
+        return redirect('home')
+
+    score = 0
+    for question in Question.objects.all():
+        answer = request.session['answers'][str(question.id)]
+        if question.is_correct(answer):
+            score += 1
+
+    score_percentage = int(score / total_questions * 100)
+    rank = get_rank(score_percentage)
+    del request.session['answers']
+
+    return render(request, 'quiz/results.html', {
+        'score': score_percentage,
+        'title': rank['title'],
+        'description': rank['description']
+    })
